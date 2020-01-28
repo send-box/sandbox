c6a44670f666ed6db977b70f1ad5979492468c42
leedothk <leedothk@gmail.com>
2020-01-28 18:28:53 +0900

    .

---
+++ yBatch2/src/main/java/com/tips/yBatch/YBatchApplication.java
@@ -0,0 +1,39 @@
+package com.tips.yBatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableBatchProcessing
+@EnableScheduling
+public class YBatchApplication {
+
+//	@Autowired
+//	JobLauncher jobLauncher;
+//	
+//	@Autowired
+//	Job processJob;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(YBatchApplication.class, args);
+	}
+
+//	@Override
+//	public void run(String... strings) throws Exception {
+//		System.out.println("---------------------- SpringBatch Start!!! ----------------------");
+//
+//		JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
+//		
+//		jobLauncher.run(processJob, jobParameters);
+//	}
+}
+
---
+++ yBatch2/src/main/java/com/tips/yBatch/step/Processor.java
@@ -0,0 +1,13 @@
+package com.tips.yBatch.step;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class Processor implements ItemProcessor<String, String>
+{
+
+	@Override
+	public String process(String data) throws Exception {
+		return data.toUpperCase();
+	}
+
+}
