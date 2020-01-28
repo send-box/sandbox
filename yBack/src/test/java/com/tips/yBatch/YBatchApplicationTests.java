3f1041d82d941bd37bb5a50cc1a0efd3448fe979
leedothk <leedothk@gmail.com>
2020-01-28 18:26:13 +0900

    .

---
+++ yBack/src/test/java/com/tips/yBatch/YBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tips.yBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
