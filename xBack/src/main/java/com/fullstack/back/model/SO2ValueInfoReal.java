54b01c8acde143753996b63a1055d1217342583b
leehyounkyoo <leehyounkyoo@gmail.com>
2020-02-03 14:54:14 +0900

    .

--- xBack/src/main/java/com/fullstack/back/model/SO2ValueInfoReal.java
+++
@@ -1,9 +0,0 @@
-package com.fullstack.back.model;
-
-public interface SO2ValueInfoReal {
-	public String getDatatime();
-	public String getSidoname();
-	public String getStationname();
-	public String getMangname();
-	public String getSo2value();
-}
